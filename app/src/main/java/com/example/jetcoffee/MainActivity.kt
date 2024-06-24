100e08409e28ead7ab0fc9bc7f332128edd18e7b
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-24 09:59:04 +0700

    feat: add component sectionText

--- app/src/main/java/com/example/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/example/jetcoffee/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,8 +22,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetcoffee.model.dummyCategory
+import com.example.jetcoffee.ui.component.CategoryItem
 import com.example.jetcoffee.ui.component.Search
 import com.example.jetcoffee.ui.theme.JetCoffeeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,6 +64,29 @@ fun Banner(
     }
 }
 
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow (
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier.padding(16.dp)
+    ) {
+        items(dummyCategory, key = {it.textCategory}) { category ->
+            CategoryItem(category)
+        }   
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+private fun CategoryRowPrev() {
+    JetCoffeeTheme {
+        CategoryRow()
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
